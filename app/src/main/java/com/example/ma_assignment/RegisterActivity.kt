17100a9211bc7de6588fbc2a7030aed1a7a7595f
Serkan Bayram <serkanbay02@hotmail.com>
2024-11-08 01:21:53 +0100

    Add routing between SignIn and Register activities

--- app/src/main/java/com/example/ma_assignment/RegisterActivity.kt
+++ app/src/main/java/com/example/ma_assignment/RegisterActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ma_assignment
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signInNowTextView = findViewById<TextView>(R.id.signInNowText)
+
+        signInNowTextView.setOnClickListener { v: View? ->
+            val intent = Intent(
+                this,
+                SignInActivity::class.java
+            )
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/ma_assignment/SignInActivity.kt
+++ app/src/main/java/com/example/ma_assignment/SignInActivity.kt
@@ -1,10 +1,15 @@
 package com.example.ma_assignment
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,17 @@ class SignInActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val registerNowTextView = findViewById<TextView>(R.id.registerNowText)
+
+        registerNowTextView.setOnClickListener { v: View? ->
+            val intent = Intent(
+                this,
+                RegisterActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
+
+
 }
